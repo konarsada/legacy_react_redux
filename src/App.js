8949c49f1639665af15c7cca432f5e79f61c4cd8
Sadabrata Konar <sadabrata.konar@airtel.com>
2024-05-25 14:26:38 +0530

    combine reducers

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import { Provider } from 'react-redux';
 import CakeContainer from './components/CakeContainer';
 import store from './redux/store';
+import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer />
+
         <CakeContainer />
+        <IceCreamContainer />
       </div>
     </Provider>
   );
